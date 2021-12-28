5a601a13a5768f78dd1aae5ecc39ba01758b0e4b
unknown <tarandeep.kaur534@gmail.com>
2021-12-28 14:24:05 +0530

    commit 1

---
+++ plot2.R
@@ -0,0 +1,12 @@
+library(downloader)
+download("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", dest="dataset.zip", mode="wb") 
+unzip ("dataset.zip", exdir = ".")
+dataFile<-"./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data$Date2 <- as.Date( as.character(data$Date),  "%d/%m/%Y")
+data2<-subset(data, Date2>=as.Date("2007-02-01") & Date2<= as.Date("2007-02-02"))
+data2$datetime <- strptime(paste(data2$Date, data2$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+data2$Global_active_power<-as.numeric(data2$Global_active_power)
+png(file="plot2.png")
+plot(data2$datetime, data2$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,26 @@
+library(downloader)
+download("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", dest="dataset.zip", mode="wb") 
+unzip ("dataset.zip", exdir = ".")
+dataFile<-"./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data$Date2 <- as.Date( as.character(data$Date),  "%d/%m/%Y")
+data2<-subset(data, Date2>=as.Date("2007-02-01") & Date2<= as.Date("2007-02-02"))
+data2$datetime <- strptime(paste(data2$Date, data2$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+png(file="plot4.png")
+
+par(mfrow=c(2,2))
+data2$Global_active_power<-as.numeric(data2$Global_active_power)
+plot(data2$datetime, data2$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+
+data2$Voltage<-as.numeric(data2$Voltage)
+plot(data2$datetime, data2$Voltage, type="l", xlab="datetime", ylab="Voltage")
+
+plot(data2$datetime, data2$Sub_metering_1, type="l", xlab="", ylab="Energy Sub Metering")
+points(data2$datetime, data2$Sub_metering_2, type="l", col="red")
+points(data2$datetime, data2$Sub_metering_3, type="l", col="blue")
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lty=c(1,1,1))
+
+data2$Global_reactive_power<-as.numeric(data2$Global_reactive_power)
+plot(data2$datetime, data2$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
