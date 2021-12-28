9014a3085eb686782a642ac05925c21ed8689edd
unknown <tarandeep.kaur534@gmail.com>
2021-12-28 15:17:06 +0530

    commit2

--- plot1.R
+++ plot1.R
@@ -1,11 +1,16 @@
+#download the file
 library(downloader)
 download("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", dest="dataset.zip", mode="wb") 
 unzip ("dataset.zip", exdir = ".")
 dataFile<-"./household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#modifications to data
 data$Date2 <- as.Date( as.character(data$Date),  "%d/%m/%Y")
 data2<-subset(data, Date2>=as.Date("2007-02-01") & Date2<= as.Date("2007-02-02"))
 data2$Global_active_power<-as.numeric(data2$Global_active_power)
+
+#plotting the data
 png(file="plot1.png")
 hist(data2$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power(Kilowatts)")
 dev.off()
--- plot3.R
+++ plot3.R
@@ -1,11 +1,16 @@
+#download the file and read it
 library(downloader)
 download("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", dest="dataset.zip", mode="wb") 
 unzip ("dataset.zip", exdir = ".")
 dataFile<-"./household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#modifications to data
 data$Date2 <- as.Date( as.character(data$Date),  "%d/%m/%Y")
 data2<-subset(data, Date2>=as.Date("2007-02-01") & Date2<= as.Date("2007-02-02"))
 data2$datetime <- strptime(paste(data2$Date, data2$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
+#plotting the data
 png(file="plot3.png")
 plot(data2$datetime, data2$Sub_metering_1, type="l", ylab="Energy Sub Metering",xlab="")
 points(data2$datetime, data2$Sub_metering_2, type="l", col="red")
